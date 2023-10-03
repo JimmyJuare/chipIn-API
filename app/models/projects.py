90f1f7fffc14e452b96a0e4e5ab1c9b1b34ac874
Jimmy Juarez <jimmyjuarez2021@gmail.com>
2023-10-03 17:24:47 -0400

    added project model

---
+++ app/models/projects.py
@@ -0,0 +1,28 @@
+from .db import db, environment, SCHEMA
+
+class Project(db.Models):
+    __tablename__ = 'Projects'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+
+    id = db.Column(db.Integer, primary_key=True)
+    project_name = db.Column(db.String(40), nullable=False)
+    project_type = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User', backref='projects')
+
+def to_dict(self):
+        return {
+            'id': self.id,
+            'project_name': self.project_name,
+            'project_type': self.project_type,
+            'user_id': self.user_id,
+            'user': {
+                'id': self.user.id,
+                'username': self.user.username,
+                'email': self.user.email,
+                # Include other user attributes as needed
+            }
+        }
