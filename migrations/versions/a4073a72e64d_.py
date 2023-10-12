2804af30cb435ca70d2843111f80935a1f072601
Jimmy Juarez <jimmyjuarez2021@gmail.com>
2023-10-12 13:43:12 -0400

    fixed

--- migrations/versions/a4073a72e64d_.py
+++ migrations/versions/a4073a72e64d_.py
@@ -8,6 +8,9 @@ Create Date: 2023-10-06 15:49:42.360339
 from alembic import op
 import sqlalchemy as sa
 
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
 revision = 'a4073a72e64d'
@@ -89,6 +92,8 @@ def upgrade():
     sa.PrimaryKeyConstraint('id')
     )
     # ### end Alembic commands ###
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
 
 
 def downgrade():
