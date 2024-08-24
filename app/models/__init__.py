b0eb8cafe6524a280a4a4329d0f064399e0dd53c
Jimmy Juarez <jimmyjuarez2021@gmail.com>
2024-08-24 01:18:59 -0500

    project table first

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,9 +1,9 @@
 from .db import db
 from .user import User
+from .project import Project
+from .posts import Post
 from .comments import Comment
 from .join_requests import JoinRequest
-from .posts import Post
 from .project_messages import ProjectMessage
-from .project import Project
 from .user_projects import UserProject
 from .db import environment, SCHEMA
